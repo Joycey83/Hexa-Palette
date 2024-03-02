4d419f3a40cfad1b2e7e911591f975f589731521
Joyce <Jcheung8385@gmail.com>
2024-03-02 17:24:49 +0000

    Added querySelector methods which are going to be used to select specific elements from the HTML document

---
+++ assets/js/rgb.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("btn");
+const spanColor = document.querySelector(".color");
+const textColor = document.querySelector(".text-color");
+const border = document.querySelector(".border");
+const boxShadow = document.querySelector(".box-shadow");
