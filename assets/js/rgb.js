652980ea4951bef715b512819f4aa11bb9cf7083
Joyce <Jcheung8385@gmail.com>
2024-03-02 17:28:53 +0000

    added the eventlistener to the btn element and invoked the randomColor() function inside the button so when the button is clicked rgb values will be generated randomly and injected into the DOM

--- assets/js/rgb.js
+++ assets/js/rgb.js
@@ -10,3 +10,13 @@ const randomColor = function () {
   console.log(rgbNum);
   return Math.floor(Math.random() * rgbNum);
 };
+
+btn.addEventListener("click", () => {
+  const rgbValue =
+    "rgb(" + randomColor() + ", " + randomColor() + ", " + randomColor() + ")";
+  spanColor.style.backgroundColor = rgbValue;
+  spanColor.textContent = rgbValue;
+  textColor.style.color = rgbValue;
+  border.style.border = `7px solid ${rgbValue}`;
+  boxShadow.style.boxShadow = `2px 2px 7px 1px ${rgbValue}`;
+});
