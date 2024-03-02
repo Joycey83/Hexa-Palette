ac5c57c335dc25b064519ac9aba39c79c7c85c32
Joyce <Jcheung8385@gmail.com>
2024-03-02 17:26:41 +0000

    added the random color function so that random rgb color values can be randomly generated when the click event is added to the button element

--- assets/js/rgb.js
+++ assets/js/rgb.js
@@ -3,3 +3,10 @@ const spanColor = document.querySelector(".color");
 const textColor = document.querySelector(".text-color");
 const border = document.querySelector(".border");
 const boxShadow = document.querySelector(".box-shadow");
+
+// RGB COLOR VALUE GENERATOR
+const randomColor = function () {
+  let rgbNum = 256;
+  console.log(rgbNum);
+  return Math.floor(Math.random() * rgbNum);
+};
