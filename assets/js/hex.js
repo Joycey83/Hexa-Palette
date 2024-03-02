0beb718cb002cf02cc18633c9ec407bcebe16f5d
Joyce <Jcheung8385@gmail.com>
2024-03-02 17:41:30 +0000

    Added querySelector methods which are going to be used to select specific html elements from the html document

--- assets/js/hex.js
+++ assets/js/hex.js
@@ -1,1 +1,7 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const spanColor = document.querySelector(".color");
+const textColor = document.querySelector(".text-color");
+const border = document.querySelector(".border");
+const boxShadow = document.querySelector(".box-shadow");
