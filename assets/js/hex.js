9703edd2daa17754b30437f7ea4edc169f7b487d
Joyce <Jcheung8385@gmail.com>
2024-03-02 17:44:44 +0000

    created a random hexcode generator function which will randomly generator 16 values from 0-9, A-F

--- assets/js/hex.js
+++ assets/js/hex.js
@@ -5,3 +5,8 @@ const spanColor = document.querySelector(".color");
 const textColor = document.querySelector(".text-color");
 const border = document.querySelector(".border");
 const boxShadow = document.querySelector(".box-shadow");
+
+// Hexcode contains 6 values but randomly generate from 16 values ranging from 0-9, A-F
+function randomHexCodeNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
